7ea9bb03f2173243fad2565665595f45314e7fbe
LAPTOP-LVSQ3KN8\宏大树的电脑 <1149522639@qq.com>
2024-04-18 21:29:15 +0800

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 3, 4, 5};
-
+        System.out.println(numbers[6]);
 
         System.out.println(numbers[5]);
     }
